22669fc76bea09c9a1b9a64099b694a48aa118b6
001sourabh <2110sourabh@gmail.com>
2021-05-31 15:09:01 +0530

    added layouts basic

--- index.js
+++ index.js
@@ -4,13 +4,20 @@ const app = express();
 const db = require('./config/mongoose');
 const port = 8000;
 
+const expressLayouts = require('express-ejs-layouts');
 app.set('view engine', 'ejs');
 app.set('views', './views');
+app.use(expressLayouts);
+
+app.set('layout extractStyles', true);
+app.set('layout extractScripts', true);
 app.use(express.static('assests'));
 // app.use(express.urlencoded());
 
 app.get('/', (req, res) => {
-    return res.render('homepage');
+    return res.render('layout', {
+        title: "xyz"
+    });
 
 })
 
