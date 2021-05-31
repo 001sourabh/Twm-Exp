75e451531ea5a5f47f425997ffc934c19df25b4c
suhas-malhotra <xyzcyz00@gmail.com>
2021-05-31 16:43:51 +0530

    navbar

--- index.js
+++ index.js
@@ -1,21 +1,21 @@
-const express = require('express');
-const path = require('path');
+const express = require("express");
+const path = require("path");
 const app = express();
-const db = require('./config/mongoose');
+const db = require("./config/mongoose");
 const port = 8000;
 
-const expressLayouts = require('express-ejs-layouts');
-app.set('view engine', 'ejs');
-app.set('views', './views');
+const expressLayouts = require("express-ejs-layouts");
+app.set("view engine", "ejs");
+app.set("views", "./views");
 app.use(expressLayouts);
 
-app.set('layout extractStyles', true);
-app.set('layout extractScripts', true);
-app.use(express.static('assests'));
+app.set("layout extractStyles", true);
+app.set("layout extractScripts", true);
+app.use(express.static("assets"));
 // app.use(express.urlencoded());
 
-app.use('/', require('./routes'));
+app.use("/", require("./routes"));
 
 app.listen(port, () => {
-    console.log(`our app listening at http://localhost:${port}`)
-})
+  console.log(`our app listening at http://localhost:${port}`);
+});
